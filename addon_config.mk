e0c6e801a39de3efa8dc3e77e29dd7802807a1c3
Christopher Baker <me@christopherbaker.net>
2015-03-07 23:45:07 -0600

    Add addon_config.mk.

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxPointer
+	ADDON_DESCRIPTION = A pointer event unification framework.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "mouse" "touch" "multitouch" "pointer"
+	ADDON_URL = http://github.com/bakercp/ofxPointer
