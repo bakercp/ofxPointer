17abfc0a7cadc94eabbe6d1da4de10edae6b539c
Christopher Baker <me@christopherbaker.net>
2015-11-24 10:49:43 -0600

    Update docs.

--- addon_config.mk
+++ addon_config.mk
@@ -1,10 +1,9 @@
 meta:
 	ADDON_NAME = ofxPointer
-	ADDON_DESCRIPTION = A pointer event unification framework.
+	ADDON_DESCRIPTION = A cross-platform pointer event unification framework.
 	ADDON_AUTHOR = bakercp
-	ADDON_TAGS = "mouse" "touch" "multitouch" "pointer"
+	ADDON_TAGS = "mouse" "touch" "multitouch" "pointer" "cross-platform" "pen"
 	ADDON_URL = http://github.com/bakercp/ofxPointer
 
 common:
 	ADDON_DEPENDENCIES = ofxJSON
-
