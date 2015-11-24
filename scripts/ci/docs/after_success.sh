aabf0e135b4580af6cdc0d10764d9cb5ff2b9e14
Christopher Baker <me@christopherbaker.net>
2015-11-24 17:15:42 -0600

    Travis updates.

--- scripts/ci/docs/after_success.sh
+++ scripts/ci/docs/after_success.sh
@@ -2,26 +2,26 @@
 set -ev
 echo "Publishing docs."
 # if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    git config --global user.name "Travis-CI"
-    git config --global user.email ${GIT_EMAIL}
-    cd docs/;
-    doxygen Doxyfile;
-    cd ..
+git config --global user.name "Travis-CI"
+git config --global user.email ${GIT_EMAIL}
+cd docs/;
+doxygen Doxyfile;
+cd ..
 
-    echo "ofxAddon documentation publishing"
+echo "ofxAddon documentation publishing"
 
-    rm -rf gh-pages || exit 0;
-    git clone --branch=gh-pages https://github.com/${TRAVIS_REPO_SLUG}.git gh-pages
-    cp -R docs/html/* gh-pages/
-    (
-    	cd gh-pages;
-    	git add --all .;
-    	git commit -m "Travis ofxAddon documentation generation.";
-    	git remote set-url origin "https://${GIT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
-    	git push -fq origin gh-pages > /dev/null 2>&1
-    )
+# rm -rf gh-pages || exit 0;
+git clone --branch=gh-pages https://github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+cp -R docs/html/* gh-pages/
+(
+	cd gh-pages;
+	git add --all .;
+	git commit -m "Travis ofxAddon documentation generation.";
+	git remote set-url origin "https://${GIT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+	git push -fq origin gh-pages > /dev/null 2>&1
+)
 
-    echo "Publishing done"
+echo "Publishing done"
 # else
 #     echo "Skipping document generation since this is a pull request.";
 # fi
